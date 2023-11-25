9197d9d3bf6a65fc1f69f54228a6e75749161fb7
gomtaengi <secndev@gmail.com>
2023-11-25 17:14:07 +0900

    add file syscall

--- system/system_server.c
+++ system/system_server.c
@@ -8,6 +8,12 @@
 #include <assert.h>
 #include <semaphore.h>
 #include <sys/shm.h>
+#include <sys/inotify.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/sysmacros.h>
+#include <dirent.h>
 
 #include <system_server.h>
 #include <gui.h>
@@ -17,6 +23,8 @@
 #include <toy_message.h>
 #include <shared_memory.h>
 
+#define BUF_LEN 1024
+#define TOY_TEST_FS "./fs"
 
 pthread_mutex_t system_loop_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t  system_loop_cond  = PTHREAD_COND_INITIALIZER;
@@ -46,7 +54,7 @@ void system_timeout_handler()
 {
     pthread_mutex_lock(&toy_timer_mutex);
     toy_timer++;
-    printf("toy_timer: %d\n", toy_timer);
+    // printf("toy_timer: %d\n", toy_timer);
     pthread_mutex_unlock(&toy_timer_mutex);
     // signal_exit();
     // printf("timer_expire_signal_handler: %d\n", toy_timer++);
@@ -130,10 +138,10 @@ void *monitor_thread(void *arg)
         if (attr.mq_curmsgs) {
             mqretcode = mq_receive(monitor_queue, (char *)&msg, sizeof(msg), NULL);
             if (mqretcode >= 0) {
-                printf("monitor_queue: 메시지가 도착했습니다.\n");
-                printf("msg.msg_type: %u\n", msg.msg_type);
-                printf("msg.param1: %u\n", msg.param1);
-                printf("msg.param2: %u\n", msg.param2);
+                // printf("monitor_queue: 메시지가 도착했습니다.\n");
+                // printf("msg.msg_type: %u\n", msg.msg_type);
+                // printf("msg.param1: %u\n", msg.param1);
+                // printf("msg.param2: %u\n", msg.param2);
                 if (msg.msg_type == SENSOR_DATA) {
                     shm_id[0] = msg.param1;
                     the_sensor_info = shmat(shm_id[0], 0, SHM_RDONLY);
@@ -141,9 +149,9 @@ void *monitor_thread(void *arg)
                         perror("shmat failed...");
                         exit(EXIT_FAILURE);
                     }
-                    printf("sensor temp: %d\n", the_sensor_info->temp);
-                    printf("sensor press: %d\n", the_sensor_info->press);
-                    printf("sensor humidity: %d\n", the_sensor_info->humidity);
+                    // printf("sensor temp: %d\n", the_sensor_info->temp);
+                    // printf("sensor press: %d\n", the_sensor_info->press);
+                    // printf("sensor humidity: %d\n", the_sensor_info->humidity);
                     if (shmdt(the_sensor_info) == -1) {
                         perror("shmdt failed...");
                         exit(EXIT_FAILURE);
@@ -155,20 +163,118 @@ void *monitor_thread(void *arg)
     return 0;
 }
 
+void display_inotify_event(int fd, int read_len, int total_size)
+{
+    struct stat sb;
+    fstat(fd, &sb);
+    
+    printf("Read %d bytes from inotify fd\n", read_len);
+    printf("directory size: %d\n", total_size);
+    printf("File type: ");
+    switch (sb.st_mode & S_IFMT) {
+    case S_IFREG:  printf("regular file\n");            break;
+    case S_IFDIR:  printf("directory\n");               break;
+    case S_IFCHR:  printf("character device\n");        break;
+    case S_IFBLK:  printf("block device\n");            break;
+    case S_IFLNK:  printf("symbolic (soft) link\n");    break;
+    case S_IFIFO:  printf("FIFO or pipe\n");            break;
+    case S_IFSOCK: printf("socket\n");                  break;
+    default:       printf("unknown file type?\n");      break;
+    }
+
+    printf("Device containing i-node: major=%ld minor=%ld\n",
+        (long)major(sb.st_dev), (long)minor(sb.st_dev));
+    printf("I-node number: %ld\n", (long)sb.st_ino);
+    printf("Number of (hard) links: %ld\n", (long)sb.st_nlink);
+    printf("Ownership:                UID=%ld   GID=%ld\n",
+            (long) sb.st_uid, (long) sb.st_gid);
+
+    printf("File size:                %lld bytes\n", (long long) sb.st_size);
+    printf("Optimal I/O block size:   %ld bytes\n", (long) sb.st_blksize);
+    printf("512B blocks allocated:    %lld\n", (long long) sb.st_blocks);
+
+    printf("Last file access: %s", ctime(&sb.st_atime));
+    printf("Last file modification: %s", ctime(&sb.st_mtime));
+    printf("Last status change: %s", ctime(&sb.st_ctime));
+}
+
+int get_dir_size()
+{
+    DIR *dirp;
+    struct dirent *dp;
+    struct stat sb;
+    int size = 0;
+    char file_name[261] = {0};
+
+    dirp = opendir(TOY_TEST_FS);
+    if (dirp == NULL) {
+        perror("opendir");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((dp = readdir(dirp))) {
+        if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
+            continue;
+        
+        sprintf(file_name, "%s/%s", TOY_TEST_FS, dp->d_name);
+        if (stat(file_name, &sb) == -1) {
+            closedir(dirp);
+            perror("stat");
+            exit(EXIT_FAILURE);
+        }
+        size += sb.st_size;
+    }
+
+    closedir(dirp);
+
+    return size;
+}
+
 void *disk_service_thread(void *arg)
 {
     char *s = arg;
-    FILE *apipe;
-    char buf[1024];
-    char cmd[] = "df -h ./";
-    int mqretcode;
-    toy_msg_t msg;
-    struct mq_attr attr;
+    // FILE *apipe;
+    // char buf[1024];
+    // char cmd[] = "df -h ./";
+    // int mqretcode;
+    // toy_msg_t msg;
+    // struct mq_attr attr;
+    int inotifyFd, wd;
+    char buf[BUF_LEN] __attribute__ ((aligned(8)));
+    ssize_t numRead;
+    struct inotify_event *event;
+    char *directory = TOY_TEST_FS;
+    char *p;
+    int total_size = 0;    
 
     printf("%s", s);
 
+    inotifyFd = inotify_init();
+    if (inotifyFd == -1) {
+        perror("inotify_init");
+        exit(EXIT_FAILURE);
+    }
+    wd = inotify_add_watch(inotifyFd, directory, IN_CREATE);
+    if (wd == -1) {
+        perror("inotify_add_watch");
+        exit(EXIT_FAILURE);
+    }
     while (1)
     {
+        numRead = read(inotifyFd, buf, BUF_LEN);
+        if (numRead == 0)
+            continue;
+        else if (numRead == -1) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        for (p=buf; p<buf+numRead;) {
+            event = (struct inotify_event*)p;
+            total_size = get_dir_size();
+            display_inotify_event(event->wd, numRead, total_size);
+            p += sizeof(struct inotify_event) + event->len;
+        }
         // apipe = popen(cmd, "r");
         // if (apipe) {
         //     while (fgets(buf, sizeof(buf), apipe))
@@ -177,17 +283,17 @@ void *disk_service_thread(void *arg)
         // } else {
         //     perror("popen() 실패...");
         // }
-        if (mq_getattr(disk_queue, &attr) == -1)
-            return 0;
-        if (attr.mq_curmsgs) {
-            mqretcode = mq_receive(disk_queue, (char *)&msg, sizeof(msg), NULL);
-            if (mqretcode >= 0) {
-                printf("disk_queue: 메시지가 도착했습니다.\n");
-                printf("msg.msg_type: %u\n", msg.msg_type);
-                printf("msg.param1: %u\n", msg.param1);
-                printf("msg.param2: %u\n", msg.param2);
-            }
-        }
+        // if (mq_getattr(disk_queue, &attr) == -1)
+        //     return 0;
+        // if (attr.mq_curmsgs) {
+        //     mqretcode = mq_receive(disk_queue, (char *)&msg, sizeof(msg), NULL);
+        //     if (mqretcode >= 0) {
+        //         printf("disk_queue: 메시지가 도착했습니다.\n");
+        //         printf("msg.msg_type: %u\n", msg.msg_type);
+        //         printf("msg.param1: %u\n", msg.param1);
+        //         printf("msg.param2: %u\n", msg.param2);
+        //     }
+        // }
     }
     return 0;
 }
