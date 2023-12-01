73d78eb58b3fe0d86ec4d52ed82a6eca092eebf9
gomtaengi <secndev@gmail.com>
2023-12-01 20:53:15 +0900

    add clone

---
+++ set_cap.sh
@@ -0,0 +1,1 @@
+sudo setcap cap_sys_admin+ep ./bin/toy_system
--- web_server/web_server.c
+++ web_server/web_server.c
@@ -1,25 +1,43 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <sched.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
 
+#define STACK_SIZE (4 * 1024 * 1024)
+
+static int web_server()
+{
+    puts("Web Server 실행합니다.");
+    execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8080", (char *) NULL);
+    return 0;
+}
+
 int create_web_server()
 {
-    pid_t systemPid;
-
     printf("여기서 Web Server 프로세스를 생성합니다.\n");
 
-    systemPid = fork();
-    switch(systemPid) {
-        case -1:
-            puts("web server fork failed...");
-            break;
-        case 0:
-            execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8080", (char *) NULL);
-            exit(EXIT_SUCCESS);
-        default:
+    char *stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
+        MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
+    if (stack == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
     }
+    int flags = SIGCHLD | CLONE_NEWPID | CLONE_NEWIPC | CLONE_NEWUTS;
+    pid_t systemPid = clone(web_server, stack + STACK_SIZE, flags, NULL);
+    if (systemPid == -1) {
+        perror("clone");
+        exit(EXIT_FAILURE);
+    }
+
+    munmap(stack, STACK_SIZE);
     return systemPid;
 }
